0af9df73ef7582eb59dfc451e2853c09b3a6b203
Maxim Abakumov <maximnatural@gmail.com>
2020-10-12 23:12:02 +0300

    добавлен тэг

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "iOSIntPackage",
-            targets: ["iOSIntPackage"]),
+            targets: ["iOSIntPackage"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "iOSIntPackageTests",
-            dependencies: ["iOSIntPackage"]),
+            dependencies: ["iOSIntPackage"])
     ]
 )
