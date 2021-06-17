c86efef0f00a8b81a14226c060094f150646a504
Maxim Abakumov <maximnatural@gmail.com>
2021-06-17 15:10:49 +0300

    process on thread

--- Sources/iOSIntPackage/ImageProcessor.swift
+++ Sources/iOSIntPackage/ImageProcessor.swift
@@ -11,6 +11,8 @@ import UIKit
 
 public struct ImageProcessor {
     
+    // MARK: - Interface
+    
     public init() {}
     
     // применение фильтра к изображению в главном потоке, не требует явного вызова DispatchQueue.main при работе с UI
@@ -36,6 +38,26 @@ public struct ImageProcessor {
             image: sourceImage,
             handler: completion)
     }
+    
+    // MARK: - Internal interface
+    
+    public func processImagesOnThread(
+        sourceImages: [UIImage],
+        filter: ColorFilter,
+        qos: QualityOfService,
+        completion: @escaping ([CGImage?]) -> Void
+    ) {
+        let thread = ImageThread(qos: qos) {
+            applyFilterOnThread(
+                sourceImages: sourceImages,
+                filter: filter,
+                completion: completion
+            )
+        }
+        thread.start()
+    }
+    
+    // MARK: - Private func
     
     private func applyFilter(
         filter: ColorFilter,
@@ -83,5 +105,31 @@ public struct ImageProcessor {
             ) else { fatalError("Error creating output image") }
             handler(outputImage)
         }
+    }
+    
+    private func applyFilterOnThread(
+        sourceImages: [UIImage],
+        filter: ColorFilter,
+        completion: @escaping ([CGImage?]) -> Void
+    ) {
+        let context = CIContext()
+        let output = sourceImages.map { image -> CGImage in
+            guard let source = CIImage(image: image) else { fatalError("Error creating source image") }
+            var params = filter.parameters
+            params[ColorFilter.imageKey] = source
+            guard let filter = CIFilter(
+                name: filter.filterName,
+                parameters: params
+            ) else { fatalError("Error creating filter") }
+            
+            guard let filteredImage = filter.outputImage else { fatalError("Error filtering image") }
+            
+            guard let outputImage = context.createCGImage(
+                filteredImage,
+                from: filteredImage.extent
+            ) else { fatalError("Error creating output image") }
+            return outputImage
+        }
+        completion(output)
     }
 }
---
+++ Sources/iOSIntPackage/ImageThread.swift
@@ -0,0 +1,24 @@
+//
+// ImageThread.swift
+// 
+// 
+// Created by Maxim Abakumov on 2021. 06. 17.
+//
+// Copyright © 2020, Maxim Abakumov. MIT License.
+//
+
+import Foundation
+
+class ImageThread: Thread {
+    
+    private var workItem: () -> Void
+    
+    init(
+        qos: QualityOfService,
+        workItem: @escaping () -> Void
+    ) {
+        self.workItem = workItem
+        super.init()
+        super.qualityOfService = qos
+    }
+}
